d9cba4ecdc2ce6d6c20b0da3e23691e8f6da8f77
Christian López García <drwn.it@gmail.com>
2025-02-24 19:45:14 +0100

    Added admin user creation for developing

---
+++ consents_api/project/users/management/commands/admin_from_env.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand
+
+from django.contrib.auth import get_user_model
+import environ
+
+env = environ.Env()
+User = get_user_model()
+
+
+class Command(BaseCommand):
+    help = "Creates a superuser from env"
+
+    def handle(self, *args, **options):
+        self.stdout.write("Admin user creation... ", ending="")
+        if User.objects.filter(is_superuser=True).exists():
+            self.stdout.write("OK")
+            return
+
+        try:
+            admin = User.objects.create_superuser(
+                username=env("ADMIN_USERNAME"),
+                password=env("ADMIN_PASSWORD"),
+            )
+            admin.save()
+            self.stdout.write("OK")
+        except Exception as e:
+            self.stderr.write(f"ERROR {e}")
