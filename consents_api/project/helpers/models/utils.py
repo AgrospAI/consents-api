457482022f6c51e1a238082da9b20b9e103ea693
Christian López García <68792480+drownedd@users.noreply.github.com>
2025-04-22 12:33:31 +0200

    chore: Removed unused code

--- consents_api/project/helpers/models/utils.py
+++
@@ -1,6 +0,0 @@
-def get_or_create(cls, get_kwargs, create_kwargs=None):
-    try:
-        return cls.objects.get(**get_kwargs)
-    except cls.DoesNotExist:
-        kwargs = create_kwargs if create_kwargs else get_kwargs
-        return cls.objects.create(**kwargs)
